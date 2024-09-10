148274c31e178eaaa4e4670b31cc3cacc1b483eb
Thies Ohme <contact@minecodes.de>
2024-09-10 20:20:49 +0200

    Fixed command & script conflict

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 if [ "$EUID" -ne 0 ]; then
     sudo pacman -Sy
-    sudo pacman -S --noconfirm reflector
+    sudo pacman -S --noconfirm reflector rsync
     sudo reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
     sudo pacman -Sy
-    sudo pacman -S --noconfirm archiso
+    sudo pacman -S --noconfirm archiso grub freetype2 fuse2 lzop efibootmgr os-probe
 else
     pacman -Sy
-    pacman -S --noconfirm reflector
+    pacman -S --noconfirm reflector rsync
     reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-    pacman -S --noconfirm archiso
+    pacman -S --noconfirm archiso grub freetype2 fuse2 lzop efibootmgr os-probe
 fi
