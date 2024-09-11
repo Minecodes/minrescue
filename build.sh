ef361945a3a122f75370c0dca35391d792bd22fd
Thies Ohme <contact@minecodes.de>
2024-09-11 21:19:22 +0200

    Fixed build script, dependency installer & missing packages

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+rm -rf work*
 mkdir -p dist work1 work2
 mkarchiso -v -w work1 -o dist full
 mkarchiso -v -w work2 -o dist minimal
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/bash
 if [ "$EUID" -ne 0 ]; then
     sudo pacman -Sy
-    sudo pacman -S --noconfirm reflector rsync
-    sudo reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-    sudo pacman -Sy
-    sudo pacman -S --noconfirm archiso grub freetype2 fuse2 lzop efibootmgr os-probe
+    sudo pacman -S --noconfirm archiso grub freetype2 fuse2 lzop efibootmgr
 else
     pacman -Sy
-    pacman -S --noconfirm reflector rsync
-    reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-    pacman -S --noconfirm archiso grub freetype2 fuse2 lzop efibootmgr os-probe
+    pacman -S --noconfirm archiso grub freetype2 fuse2 lzop efibootmgr
 fi
